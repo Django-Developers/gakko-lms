7038ab7afb5c4934d24a94295e7bf0bd15354736
nilu70 <you@example.com>
2021-02-01 20:13:57 +0330

    User_form_login

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 from user.models import User
 
 
@@ -22,3 +23,17 @@ class ExtendedUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+
+class UserAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label='password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields=('email','password')
+
+    def clean(self):
+        email= self.cleaned_data['email']
+        password = self.cleaned_data['password']
+        if not authenticate(email=email,password=password):
+            raise forms.ValidationError("Invalid login")
