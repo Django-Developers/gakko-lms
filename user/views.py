63a3f3a7733b082fdcf9dd74ac399f075b908316
nilu70 <you@example.com>
2021-02-01 20:33:20 +0330

    login_view

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 #from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
-from .forms import ExtendedUserCreationForm
+from .forms import ExtendedUserCreationForm, UserAuthenticationForm
 
 def index(request):
     if request.user.is_authenticated:
@@ -42,3 +42,23 @@ def logout_view(request):
     return redirect ('home')
 
 
+def login_view(request):
+    context={}
+
+    user= request.user
+    if user.is_authenticated:
+        return redirect("home")
+    if request.POST:
+        form = UserAuthenticationForm(request.POST)
+        if form.is_valid():
+            email=request.POST['email']
+            password=request.POST['password']
+            user=authenticate(email=email,password=password)
+
+            if user:
+                login(request,user)
+                return redirect("home")
+    else:
+        form= UserAuthenticationForm()
+    context['login_form']=form
+    return render(request,'user/login.html',context)
